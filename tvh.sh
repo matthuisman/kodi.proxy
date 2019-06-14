f095213eae2be7569e7a6727b7938bbdcebc1f98
Matt Huisman <me@matthuisman.nz>
2019-06-14 12:46:53 +1200

    Get dir

--- tvh.sh
+++ tvh.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-OUTPUT="$(PROXY_TYPE=TVH DEBUG=0 ./proxy.py $1)"
+scriptdir=$(dirname "$0")
+OUTPUT="$(PROXY_TYPE=TVH DEBUG=0 $scriptdir/proxy.py $1)"
 
 if [ $? -eq 200 ]; then
     eval "ffmpeg $OUTPUT"
