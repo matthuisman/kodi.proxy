c9dfbd529fb939c75a1d425a83f07cb93357ea2d
Matt Huisman <hi@matthuisman.nz>
2019-07-05 13:05:32 +1200

    clean

--- tvh.sh
+++ tvh.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-OUTPUT="$(PROXY_TYPE=TVH DEBUG=0 $SCRIPT_PATH/.env/bin/python $SCRIPT_PATH/proxy.py $1)"
+OUTPUT="$(PROXY_TYPE=TVH $SCRIPT_PATH/.env/bin/python $SCRIPT_PATH/proxy.py $1)"
 
 if [ $? -eq 200 ]; then
     eval "ffmpeg $OUTPUT"
