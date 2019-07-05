a0f0e00ceb094100d89eb9eddaf063a7ba191600
Matt Huisman <me@matthuisman.nz>
2019-07-05 19:35:45 +1200

    Fix local thumb paths

--- playlist_iptv_merge.sh
+++ playlist_iptv_merge.sh
@@ -5,4 +5,4 @@ SCRIPT_PATH="$(dirname $SCRIPT)"
 
 PLAYLIST_PATH="$(PROXY_TYPE=TV_GRAB $SCRIPT_PATH/.env/bin/python $SCRIPT_PATH/proxy.py 'plugin://plugin.program.iptv.merge/?_=proxy_merge&type=playlist')"
 
-cat "$PLAYLIST_PATH" | sed -E "s|(^plugin://.*?$)|pipe://$SCRIPT_PATH/tvh.sh \"\1\"|g"
+cat "$PLAYLIST_PATH" | sed -E "s|(^plugin://.*?$)|pipe://$SCRIPT_PATH/tvh.sh \"\1\"|g" | sed -E "s|tvg-logo=\"(/.*?)\"|tvg-logo=\"file://\1\"|g"
