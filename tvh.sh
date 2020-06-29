913318b94bf44f7535618b34376851e6de95a7a7
Matt Huisman <hi@matthuisman.nz>
2020-06-30 08:58:26 +1200

    update tvh cmd

--- tvh.sh
+++ tvh.sh
@@ -7,7 +7,7 @@ OUTPUT="$(proxy_type=TVH $SCRIPT_PATH/.env/bin/python $SCRIPT_PATH/proxy.py $1)"
 
 if [ $? -eq 200 ]; then
     shift
-    eval "ffmpeg $OUTPUT $@ -vcodec copy -acodec copy -f mpegts -tune zerolatency pipe:1"
+    eval "ffmpeg $OUTPUT $@ -vcodec copy -acodec copy -f mpegts pipe:1"
 else
     echo "$OUTPUT"
 fi
