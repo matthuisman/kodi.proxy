03b3389604e3804eb7ccadc7cf46f6779fdddb0c
Matthew Huisman <gitlab@matthuisman.nz>
2021-05-13 20:05:21 +1200

    Support latest common

--- iptv_merge.m3u8.sh
+++ iptv_merge.m3u8.sh
@@ -3,7 +3,7 @@
 SCRIPT="$(realpath $0)"
 SCRIPT_PATH="$(dirname $SCRIPT)"
 
-PLAYLIST_PATH="$(proxy_type=TV_GRAB $SCRIPT_PATH/.env/bin/python $SCRIPT_PATH/proxy.py 'plugin://plugin.program.iptv.merge/?_=proxy_merge&type=playlist')"
+PLAYLIST_PATH="$(proxy_type=TV_GRAB $SCRIPT_PATH/.env/bin/python $SCRIPT_PATH/proxy.py 'plugin://plugin.program.iptv.merge/?_=proxy_merge&type=playlist' | cut -c 2-)"
 
 if [ $? -eq 200 ]; then
     cat "$PLAYLIST_PATH" | sed -E "s|(^plugin://.*?$)|pipe://$SCRIPT_PATH/tvh.sh \"\1\"|g" | sed -E "s|tvg-logo=\"(/.*?)\"|tvg-logo=\"file://\1\"|g"
